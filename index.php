d4417f7779127a0a854fbb1af62508c5807cbe22
oguzbilgic <fisyonet@gmail.com>
2010-07-17 01:17:12 +0000

    Unnecessary require_once functions are removed

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?
 require_once 'hivli/Library.php';
-require_once 'hivli/Auth.php';
 require_once 'bootstrap/Multi.php';
-require_once 'bootstrap/Multi/View.php';
-
 
 $bootstrap = new Core_Bootstrap_Multi;
 $bootstrap->run();
