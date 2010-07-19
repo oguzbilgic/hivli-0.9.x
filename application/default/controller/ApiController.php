58b9fffdcd241bb24e32374472c7d056969934e1
oguzbilgic <fisyonet@gmail.com>
2010-07-19 02:28:13 +0000

    Added hivli.controller.abstract.api example to application

--- application/default/controller/ApiController.php
+++ application/default/controller/ApiController.php
@@ -3,6 +3,5 @@ class ApiController extends Hivli_Controller_Abstract_Api {
 	
 	function IndexAction(){
 		$this->view->response = array('response' => 'ok', 'data' => array('title', 'name', 'user'));
-		Hivli::get('View')->setType('json');
 	}
 }
