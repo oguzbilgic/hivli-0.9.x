dc1454df79a3bdaad912ddf9391176ca87ff71bb
oguzbilgic <fisyonet@gmail.com>
2010-07-31 06:00:24 +0300

    Moved files of the library folder into application/default/model folder

--- bootstrap/Multi/Model.php
+++ bootstrap/Multi/Model.php
@@ -1,9 +1,8 @@
 <?
 class Core_Bootstrap_Multi_Model extends Core_Bootstrap_Multi_Abstract {
 	
-	function postDetectApp(){
-		Hivli::get('Model')->addModelPath('library/');
-		Hivli::get('Model')->addModelPath('hivli/');
+	function postDetectApp(){		
+		Hivli::get('Model')->addModelPath('application/' . Hivli::get('Router')->getAppFolder() . 'model/');
 		set_include_path(get_include_path() . PATH_SEPARATOR . 'library/'. PATH_SEPARATOR . 'hivli/');
 	}	
 }
