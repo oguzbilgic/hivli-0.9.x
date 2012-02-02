980969b31dd3fe84ba03598b77f529baa23f6af5
Oguz Bilgic <fisyonet@gmail.com>
2012-02-02 01:37:55 -0800

    Move index.php and .htaccess to public folder

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?
-require_once 'hivli/Library.php';
-require_once 'bootstrap/Auth.php';
-require_once 'bootstrap/Database.php';
-require_once 'bootstrap/I18n.php';
+require_once '../hivli/Library.php';
+require_once '../bootstrap/Auth.php';
+require_once '../bootstrap/Database.php';
+require_once '../bootstrap/I18n.php';
 
 $bootstrap = new Hivli_Bootstrap;
 //$bootstrap->addPlugin(new Bootstrap_Auth());
