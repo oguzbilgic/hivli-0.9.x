3255b57d0b79dd1b37a1c356c43c5aec25681c9a
Oguz Bilgic <fisyonet@gmail.com>
2012-02-02 01:44:32 -0800

    Retab Html view helper class

--- hivli/View/Helper/html.php
+++ hivli/View/Helper/html.php
@@ -1,30 +1,30 @@
 <?
 class Hivli_View_Helper_Html {
-	
-	function css($cssFileName){
-		echo '<link rel="stylesheet" type="text/css" href="http://' . $_SERVER["SERVER_NAME"] . '/'. Hivli::get('View')->getsitePath() . 
-				'public/' . Hivli::get('View')->getPublicViewPath() . 'css/' . $cssFileName . '.css" media="screen" />';
-	}
-	
-	function js($jsFileName, $version = null){
-		echo '<script type="text/javascript" src="http://' . $_SERVER["SERVER_NAME"] . '/' . Hivli::get('View')->getsitePath() . 'public/'. 
-				Hivli::get('View')->getPublicViewPath() . 'js/' . $jsFileName . '.js' . $version . '"/>';
-	}
-	
-	function image($url){
-		return 'http://' . $_SERVER['SERVER_NAME'] . '/' . Hivli::get('View')->getsitePath() . 'public/' . Hivli::get('View')->getPublicViewPath() . 'images/' . $url;
-	}
-	
-	function uri($uri){
-		$uri = str_replace(' ', '-', $uri);
-		return 'http://' . $_SERVER['SERVER_NAME'] . '/' . Hivli::get('View')->getsitePath() . $uri;
-	}
-	
-	function googleJs($jsLibrary){
-		switch($jsLibrary){
-			case 'jquery':
-				echo '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"/>';
-				break;
-		}
-	}
+  
+  function css($cssFileName){
+    echo '<link rel="stylesheet" type="text/css" href="http://' . $_SERVER["SERVER_NAME"] . '/'. Hivli::get('View')->getsitePath() . 
+        'public/' . Hivli::get('View')->getPublicViewPath() . 'css/' . $cssFileName . '.css" media="screen" />';
+  }
+  
+  function js($jsFileName, $version = null){
+    echo '<script type="text/javascript" src="http://' . $_SERVER["SERVER_NAME"] . '/' . Hivli::get('View')->getsitePath() . 'public/'. 
+        Hivli::get('View')->getPublicViewPath() . 'js/' . $jsFileName . '.js' . $version . '"/>';
+  }
+  
+  function image($url){
+    return 'http://' . $_SERVER['SERVER_NAME'] . '/' . Hivli::get('View')->getsitePath() . 'public/' . Hivli::get('View')->getPublicViewPath() . 'images/' . $url;
+  }
+  
+  function uri($uri){
+    $uri = str_replace(' ', '-', $uri);
+    return 'http://' . $_SERVER['SERVER_NAME'] . '/' . Hivli::get('View')->getsitePath() . $uri;
+  }
+  
+  function googleJs($jsLibrary){
+    switch($jsLibrary){
+      case 'jquery':
+        echo '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"/>';
+        break;
+    }
+  }
 }
